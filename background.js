76c5e525f3bc48915ce568297a038444e36b4339
Leo Zhang <leozhang2024@gmail.com>
2025-10-25 12:40:10 -0700

    Removed redundant code: Event listener for click is unecessary because manifest already opens default_popup

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
 import { check } from './groq.js';
 
-chrome.action.onClicked.addListener((tab) => {
-  if (!tab.id) return;
-  console.log("Extension clicked");
-  chrome.tabs.sendMessage(tab.id, { action: "factCheck" });
-});
-
 // Listen for fact-check requests from content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "performFactCheck") {
